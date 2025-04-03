33e73290260699ecbaaa18c52cd0538ee2d1bdd8
brandonhach <specialcambo@gmail.com>
2025-04-03 14:06:34 -0400

    🎉 (chore) initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { FormComponent } from './pages/form/form.component';
+import { ReactiveFormComponent } from './pages/reactive-form/reactive-form.component';
+import { DynamicFormComponent } from './pages/dynamic-form/dynamic-form.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'form', component: FormComponent },
+  { path: 'reactive-form', component: ReactiveFormComponent },
+  { path: 'dynamic-form', component: DynamicFormComponent },
+  { path: '', redirectTo: '', pathMatch: 'full' },
+];
