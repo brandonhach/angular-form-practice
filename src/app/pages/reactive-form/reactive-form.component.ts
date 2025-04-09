94aa9627ebbcdbd0294036560bcc23d23597d2c5
brandonhach <specialcambo@gmail.com>
2025-04-09 11:54:43 -0400

    (example) reactive form

--- src/app/pages/reactive-form/reactive-form.component.ts
+++ src/app/pages/reactive-form/reactive-form.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './reactive-form.component.html',
-  styleUrl: './reactive-form.component.css'
+  styleUrl: './reactive-form.component.css',
 })
 export class ReactiveFormComponent {
+  loginForm: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
 
+  constructor() {}
+
+  /**
+   * submitform
+   * With Reactive Form, you can see certain additional information
+   * If you just need the value, simply use property "value"
+   * @param event
+   */
+  submitForm(event: Event) {
+    event.preventDefault();
+    console.log(`Form submitted`);
+    console.log(this.loginForm.value);
+  }
 }
