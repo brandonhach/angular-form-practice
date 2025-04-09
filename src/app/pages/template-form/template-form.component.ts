af63684bdd929fa8051ee28c95740faf80fd5872
brandonhach <specialcambo@gmail.com>
2025-04-09 11:43:23 -0400

    example of template form

--- src/app/pages/template-form/template-form.component.ts
+++ src/app/pages/template-form/template-form.component.ts
@@ -1,11 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-template-form',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './template-form.component.html',
-  styleUrl: './template-form.component.css'
+  styleUrl: './template-form.component.css',
 })
 export class TemplateFormComponent {
+  username: string = '';
+  password: string = '';
 
+  constructor() {}
+
+  /**
+   * submitform
+   *
+   * @param event
+   */
+  submitForm(event: Event) {
+    event.preventDefault();
+    console.log(`Form submitted: ${event}`);
+    console.log(`Username: ${this.username} Password: ${this.password}`);
+  }
 }
