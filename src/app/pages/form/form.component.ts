9eb09f655a13a97c0552d4571934441c64969f94
brandonhach <specialcambo@gmail.com>
2025-04-09 11:25:27 -0400

    basic example of a reg form

--- src/app/pages/form/form.component.ts
+++ src/app/pages/form/form.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-form',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './form.component.html',
-  styleUrl: './form.component.css'
+  styleUrl: './form.component.css',
 })
 export class FormComponent {
+  constructor() {}
 
+  /**
+   * submitform
+   * Returns an event but is not destructured (just a obj with no way to decipher)
+   * @param event
+   */
+  submitForm(event: Event) {
+    event.preventDefault();
+    console.log(`Form submitted: ${event}`);
+  }
 }
